13ab5fddbce92a79e949a3bf3285463a4d43ea23
yisichoco <yisichoco@gmail.com>
2026-06-01 15:32:25 +0800

    Props屬性-PC關係-屬性傳值

--- app/ch5-props/pc/_components/child.tsx
+++ app/ch5-props/pc/_components/child.tsx
@@ -1,6 +1,24 @@
 'use client';
 
-export default function Child() {
+import { ReactNode } from 'react';
+
+interface Data {
+  x?: number;
+  y?: number;
+}
+
+// props
+interface ChildProps {
+  title?: string;
+  price?: number;
+  isConnected?: boolean;
+  aa?: number[];
+  oa?: Data;
+  sum?: (a: number, b: number) => number;
+  jsx?: ReactNode;
+}
+
+export default function Child(props: ChildProps) {
   return (
     <>
       <div>Child(子女元件)</div>
--- app/ch5-props/pc/_components/parent.tsx
+++ app/ch5-props/pc/_components/parent.tsx
@@ -9,7 +9,15 @@ export default function Parent() {
       {/* 由渲染定義的P-C關係 */}
       {/* P-C關係是由"P(父母)"渲染"C(子女)"來決定 */}
       {/* P元件可以利用類似HTML屬性值的指定方式，將值傳遞到C元件中，但不限於字串值 */}
-      <Child />
+      <Child
+        title="目前正在教第5章"
+        price={100}
+        isConnected={false}
+        aa={[1, 2, 3]}
+        oa={{ x: 1, y: 2 }}
+        sum={(a, b) => a + b}
+        jsx={<p>hello</p>}
+      />
     </>
   );
 }
