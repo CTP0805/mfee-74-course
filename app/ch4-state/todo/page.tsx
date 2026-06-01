790ac2223a0dc4addbcc75faade04682804edfe1
yisichoco <yisichoco@gmail.com>
2026-06-01 11:26:17 +0800

    State狀態-待辨事項-新增

--- app/ch4-state/todo/page.tsx
+++ app/ch4-state/todo/page.tsx
@@ -5,17 +5,17 @@ import { useState } from 'react';
 // 範例資料(mock)
 const initData = [
   {
-    id: 1,
+    id: 'u001',
     text: '繳電信費',
   },
-  { id: 2, text: '寫作業' },
+  { id: 'u002', text: '寫作業' },
 ];
 
 export default function TodoPage() {
   // 記錄待辨事項的狀態
   const [todos, setTodos] = useState(initData);
   // 宣告給文字輸入框使用的狀態
-  const [inputText, setInputText] = useState('aaa');
+  const [inputText, setInputText] = useState('');
 
   return (
     <>
@@ -28,6 +28,24 @@ export default function TodoPage() {
         // 在文字輸入框輸入了什麼 -> 狀態更動成什麼
         onChange={(e) => {
           setInputText(e.target.value);
+        }}
+        // 按下enter鍵要觸發加入到todos
+        onKeyDown={(e) => {
+          // 按下enter鍵而且有輸入文字的情況下
+          if (e.key === 'Enter' && inputText.trim()) {
+            // 建立新的todo
+            const newTodo = {
+              id: crypto.randomUUID(), //新項目使用uuid當作id
+              text: inputText,
+            };
+            // 狀態更動第1、2步。加到新狀態的最前面
+            const nextTodos = [newTodo, ...todos];
+            // 狀態更動第3步
+            setTodos(nextTodos);
+
+            // 清空文字輸入框
+            setInputText('');
+          }
         }}
       />
       <ul>
