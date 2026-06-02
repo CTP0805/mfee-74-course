700906f7a57dbea5b8432e9234e4fcd5184f2b5c
yisichoco <yisichoco@gmail.com>
2026-06-02 10:19:12 +0800

    Props屬性-children範例-結構

---
+++ app/ch5-props/children/_components/child.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+
+export interface ChildProps {}
+
+export default function Child({}: ChildProps) {
+  return (
+    <>
+      <div>Child</div>
+    </>
+  );
+}
---
+++ app/ch5-props/children/_components/parent.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+
+export interface ParentProps {
+  
+}
+
+export default function Parent({  }: ParentProps) {
+  return (
+    <>
+      <div>Parent</div>
+    </>
+  );
+}
