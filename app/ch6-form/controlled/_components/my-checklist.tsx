28ef8eac0a0c1580ee14aeebb26edb769b0809c5
yisichoco <yisichoco@gmail.com>
2026-06-08 16:15:05 +0800

    Form&Refs表單_可控表單元件-CheckList補充全選

--- app/ch6-form/controlled/_components/my-checklist.tsx
+++ app/ch6-form/controlled/_components/my-checklist.tsx
@@ -23,7 +23,7 @@ export default function MyChecklist() {
 
   // 處理核取方塊的勾選
   const onToggleChecked = (petId: number) => {
-    const nexPets = pets.map((pet) => {
+    const nextPets = pets.map((pet) => {
       // 對符合條件的物件作修改
       if (pet.id === petId) {
         // 用展開運算子進行複製物件，並修改它的checked值(true <--> false
@@ -33,13 +33,31 @@ export default function MyChecklist() {
       }
     });
     // 設定回狀態
-    setPets(nexPets);
+    setPets(nextPets);
+  };
+
+  const onToggleCheckedAll = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // 強制讓所有的pets中的每個pet物件的checked都和全選一致
+    const nextPets = pets.map((pet) => {
+      return { ...pet, checked: e.target.checked };
+    });
+    // 設定回狀態
+    setPets(nextPets);
   };
 
   return (
     <>
       <h2>核取方塊群組</h2>
       <hr />
+      <input
+        type="checkbox"
+        // 每個pet狀態中的checked的總合(布林值AND)
+        checked={pets.every((v) => v.checked)}
+        onChange={onToggleCheckedAll}
+      />{' '}
+      全選
+      <br />
+      <br />
       {pets.map((pet) => {
         return (
           <label key={pet.id}>
